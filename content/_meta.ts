bbc25af805aa8c8ef70127370f0780a3673d9587
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 17:44:52 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,10 +1,5 @@
 export default {
   'index': { display: 'hidden' },
-  'api-valid-jolli-api-yaml': {
-    title: 'Valid - Jolli API (YAML)',
-    type: 'page',
-    href: '/api-docs-valid-jolli-api-yaml.html'
-  },
   'api-reference': {
     title: 'API Reference',
     type: 'page',
